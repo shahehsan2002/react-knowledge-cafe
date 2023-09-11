488ff6402525b0d6752eee52a2419003eedcc16e
shahehsan2002 <shahehsan2002@.com>
2023-09-12 02:16:51 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+             <h1 className='text-4xl font-bold text-center '>Knowledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;      
